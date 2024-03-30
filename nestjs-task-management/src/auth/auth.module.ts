07eb4d3c1d4e1e4244a1f36b9975a08a6d06c506
Miad Hasan <mdhasan9.sy@gmail.com>
2024-03-30 15:13:44 +0600

    Added config for jwt token

--- nestjs-task-management/src/auth/auth.module.ts
+++ nestjs-task-management/src/auth/auth.module.ts
@@ -6,17 +6,23 @@ import { User } from './user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+    }),
+    ConfigModule,
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'jwtsecret',
-      signOptions: {
-        expiresIn: 3600,
-      },
-    }),
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
