420369281ecd284d0ad5bce72c6b7260cabc6d27
Miad Hasan <mdhasan9.sy@gmail.com>
2024-03-30 15:43:20 +0600

    restructure files

---
+++ Day-7_Nest/nestjs-task-management/src/tasks/tasks.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+import { Task } from './task.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
+  controllers: [TasksController],
+  providers: [TasksService],
+})
+export class TasksModule {}
