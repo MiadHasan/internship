f39b3f48791e2c40fa0226f4e4c57cf357073aa9
Miad Hasan <mdhasan9.sy@gmail.com>
2024-03-30 22:11:44 +0600

    Basic project structure

--- Day-8_Nest/dev-hub/src/app.module.ts
+++ Day-8_Nest/dev-hub/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DevModule } from './dev/dev.module';
+import { UserModule } from './user/user.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [DevModule, UserModule, PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
